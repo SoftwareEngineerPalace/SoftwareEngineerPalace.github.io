63576729b11a7118a958e01fa58c41a25cde9125
xiaojianjun <jianjun.xiao@foxmail.com>
2023-04-05 20:05:07 +0800

    a

--- src/src/main.js
+++ src/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(Antd).mount("#app");
---
+++ src/src/router/modules/base.js
@@ -0,0 +1,14 @@
+import Task from "@/pages/Task.vue";
+import Gate from "@/pages/Gate.vue";
+export default [
+  {
+    path: "/task",
+    name: "task",
+    component: Task,
+  },
+  {
+    path: "/gate",
+    name: "gate",
+    component: Gate,
+  },
+];
